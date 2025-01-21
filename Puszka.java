e0f9b12bca035fe301db93b54d251c715e3a6ae1
rs-projop <p10radzio2008s3@gmail.com>
2025-01-21 19:55:33 +0100

    Dodanie komentarzy JavaDoc

--- Puszka.java
+++ Puszka.java
@@ -1,9 +1,23 @@
+/**
+ * Klasa obsługująca zapisywanie zwykłych puszek
+ */
+
 public class Puszka {
     String firma;
     String smak;
     int mililitry;
     double cena;
     boolean cukier;
+
+    /**
+     * Konstruktor tworzący obiekt z danymi zwykłej puszki
+     *
+     * @param firma Nazwa firmy
+     * @param smak Smak napoju
+     * @param mililitry Ilość mililitrów
+     * @param cena Cena napoju
+     * @param cukier Czy zawiera cukier
+     */
 
     public Puszka(String firma, String smak, int mililitry, double cena, boolean cukier) {
         this.firma = firma;
@@ -13,25 +27,59 @@ public class Puszka {
         this.cukier = cukier;
     }
 
+    /**
+     * Zwraca nazwę firmy
+     *
+     * @return Nazwa firmy
+     */
+
     public String getFirma() {
         return firma;
     }
+
+    /**
+     * Zwraca smak napoju
+     *
+     * @return Smak napoju
+     */
 
     public String getSmak() {
         return smak;
     }
 
+    /**
+     * Zwraca ilość mililitrów
+     *
+     * @return Ilość mililitrów
+     */
+
     public int getMililitry() {
         return mililitry;
     }
+
+    /**
+     * Zwraca cenę napoju
+     *
+     * @return Cena napoju
+     */
 
     public double getCena() {
         return cena;
     }
 
+    /**
+     * Zwraca czy napój zawiera cukier
+     *
+     * @return True lub false w zależności od obecności cukru
+     */
+
     public boolean isCukier() {
         return cukier;
     }
+
+    /**
+     * Wypisuje informacje o nowej puszce
+     */
 
     public void nowaPuszka() {
         System.out.println("Dodano nową puszkę: Napój " + firma + " o smaku " + smak + "!");
